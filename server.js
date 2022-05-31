3ebd9bbd120cb7b18505a529d8c7b902163d1e54
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2022-05-31 13:01:23 -0500

    bug fixed issue where filter was not returning properly, added commentation

--- server.js
+++ server.js
@@ -13,46 +13,63 @@ app.use(express.urlencoded({
 
 app.use(express.static('public'))
 
+//directs start screen to the notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
+//pulls the notes from the db.json file
 app.get('/api/notes', (req, res) => {
     var notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
     res.json(notes)
 })
 
+//posts new notes from the save icon
 app.post('/api/notes', (req, res) => {
     var notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
+    //combines body to var
     var newNote = req.body;
 
+    //gives note a unique id via module
     newNote.id = uuid()
 
+    //adds to the array
     var noteArray  = [...notes, newNote]
     
+    //writes new array to db file
     fs.writeFileSync('./db/db.json', JSON.stringify(noteArray))
 
+    //pulls the new array to the user interface
     res.json(noteArray)
 })
 
+//deletes note on trash icon click
 app.delete('/api/notes/:id', (req, res) => {
+    //stores id of attached note
     var tbDelete = req.params.id
 
+    //parses for editing
     var notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
     
+    //edits array to filter for all id's that do not match tbDelete
     var filteredNotes = notes.filter(note => {
-        note != tbDelete
+        console.log(note.id, tbDelete)
+        return note.id != tbDelete
     })
 
+    //edits db.json file
     fs.writeFileSync('./db/db.json', JSON.stringify(filteredNotes))
 
+    //pulls new array to the user interface
     res.json(filteredNotes)
 })
 
+//if no location specified, take user to start screen
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
+//opens port
 app.listen(PORT, () => {
     console.log(`listening on PORT: ${PORT}`)
 })
